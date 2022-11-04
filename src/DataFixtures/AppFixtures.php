c0881f6f00bb40d2f890176e2beb47200f4151ca
Lucas Delobelle <lucas@ld-web.net>
2022-11-04 06:54:32 +0100

    :tada: initial commit
    Doctrine, Profiler, Maker, Fixtures, User entity

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+  public function __construct(private UserPasswordHasherInterface $passwordHasher)
+  {
+  }
+
+  public function load(ObjectManager $manager): void
+  {
+    $user = new User();
+
+    $user->setEmail('bobby@bob.com')
+      ->setPassword($this->passwordHasher->hashPassword($user, "bobby1234"));
+
+    $manager->persist($user);
+    $manager->flush();
+  }
+}
